e93b378e8a0614cc5a1388cc54e546b00ef4b8d2
suzhida <jeroem_s@qq.com>
2016-06-16 11:39:30 +0800

    SipmleDateFormat不是线程安全的，多线程下面可能出现问题
    Signed-off-by: suzhida <jeroem_s@qq.com>

---
+++ src/main/java/threadSecurity/SipmleDateFormatSecurityOK.java
@@ -0,0 +1,74 @@
+package threadSecurity;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 
+ * 创建多个SimpleDateFormat类的实例解决多线程错误的问题
+ * 
+ * @author suzhida
+ */
+public class SipmleDateFormatSecurityOK {
+	public static void main(String[] args) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		// 9个数组
+		String[] dateStringArr = new String[]{
+				"2016-05-16",
+				"2016-05-17",
+				"2016-05-18",
+				"2016-05-19",
+				"2016-05-20",
+				"2016-05-21",
+				"2016-05-22",
+				"2016-05-23",
+				"2016-05-24"
+			};
+
+		MyThreadOK[] threadArr = new MyThreadOK[9];
+		for (int i = 0; i < threadArr.length; i++) {
+			threadArr[i] = new MyThreadOK(sdf, dateStringArr[i]);
+		}
+
+		for (int i = 0; i < threadArr.length; i++) {
+			threadArr[i].start();
+		}
+	}
+}
+
+class MyThreadOK extends Thread {
+	private SimpleDateFormat sdf;
+	private String dateString;
+
+	public MyThreadOK(SimpleDateFormat sdf, String dateString) {
+		super();
+		this.sdf = sdf;
+		this.dateString = dateString;
+	}
+
+	@Override
+	public void run() {
+		try {
+			// Date dateRef = sdf.parse(dateString);
+			Date dateRef = DateTools.parse("yyyy-MM-dd", dateString);
+			String newDateString = DateTools.format("yyyy-MM-dd", dateRef).toString();
+			if (!newDateString.equals(dateString)) {
+				System.out.println(
+						"ThreadName=" + this.getName() + "报错了 日期字符串：" + dateString + "转化成的日期为：" + newDateString);
+			}
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class DateTools {
+	public static Date parse(String formatPattern, String dateString) throws ParseException {
+		return new SimpleDateFormat(formatPattern).parse(dateString);
+	}
+
+	public static String format(String formatPattern, Date date) {
+		return new SimpleDateFormat(formatPattern).format(date).toString();
+	}
+}
---
+++ src/main/java/threadSecurity/SipmleDateFormatSecurityOK2.java
@@ -0,0 +1,79 @@
+package threadSecurity;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 
+ * ThreadLocal能使多线程绑定到指定的对象，使用该类可以解决多线程环境下SimpleDateFormat类处理错误的问题
+ * 
+ * @author suzhida
+ */
+public class SipmleDateFormatSecurityOK2 {
+	public static void main(String[] args) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		// 9个数组
+		String[] dateStringArr = new String[]{
+				"2016-05-16",
+				"2016-05-17",
+				"2016-05-18",
+				"2016-05-19",
+				"2016-05-20",
+				"2016-05-21",
+				"2016-05-22",
+				"2016-05-23",
+				"2016-05-24"
+			};
+
+		MyThreadOK2[] threadArr = new MyThreadOK2[9];
+		for (int i = 0; i < threadArr.length; i++) {
+			threadArr[i] = new MyThreadOK2(sdf, dateStringArr[i]);
+		}
+		for (int i = 0; i < threadArr.length; i++) {
+			threadArr[i].start();
+		}
+	}
+}
+
+class MyThreadOK2 extends Thread {
+	private SimpleDateFormat sdf;
+	private String dateString;
+
+	public MyThreadOK2(SimpleDateFormat sdf, String dateString) {
+		super();
+		this.sdf = sdf;
+		this.dateString = dateString;
+	}
+
+	@Override
+	public void run() {
+		try {
+			// Date dateRef = sdf.parse(dateString);
+			Date dateRef = DateTools2.getSimpleDateFormat("yyyy-MM-dd").parse(dateString);
+			String newDateString = DateTools2.getSimpleDateFormat("yyyy-MM-dd").format(dateRef).toString();
+			if (!newDateString.equals(dateString)) {
+				System.out.println(
+						"ThreadName=" + this.getName() + "报错了 日期字符串：" + dateString + "转化成的日期为：" + newDateString);
+			}
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class DateTools2 {
+	private static ThreadLocal<SimpleDateFormat> tl = new ThreadLocal<SimpleDateFormat>();
+
+	public static SimpleDateFormat getSimpleDateFormat(String datePattern) {
+		SimpleDateFormat sdf = null;
+		sdf = tl.get();
+		if (sdf == null) {
+			sdf = new SimpleDateFormat(datePattern);
+			tl.set(sdf);
+		}
+		return sdf;
+	}
+}
