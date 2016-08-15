32e399b3dcb28351eebfe60c377c2e7cbeb8b38f
suzhida <jerome_s@qq.com>
2016-08-15 23:32:17 +0800

    新增 groovytest

---
+++ src/main/java/groovy/GroovyTest.java
@@ -0,0 +1,28 @@
+package groovy;
+
+//import groovy.lang.Binding;
+//import groovy.lang.GroovyShell;
+//
+//import org.junit.Test;
+
+public class GroovyTest {
+
+//	@Test
+//	public void test(){
+//		
+//		Binding binding = new Binding();
+//
+//		binding.setVariable("var", 5);
+//
+//		GroovyShell gs = new GroovyShell(binding);
+//
+//		Object value = gs.evaluate("println 'Hello Groovy !';abc=123;return var*10");//执行groovyshell脚本
+//		
+//		Object valu1e = gs.evaluate("println 'This is ${i}:${val}'");//执行groovyshell脚本
+//
+//		System.out.println(value.equals(50));
+//
+//		System.out.println(binding.getVariable("abc").equals(123));
+//		
+//	}
+}
